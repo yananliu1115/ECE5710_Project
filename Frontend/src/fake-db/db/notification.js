63f204486fba7d3bc25d571a393a9e715173a8a2
yn <yl2248@cornell.edu>
2022-04-14 20:13:51 -0400

    chore: deleted notifications message

--- Frontend/src/fake-db/db/notification.js
+++ Frontend/src/fake-db/db/notification.js
@@ -3,42 +3,42 @@ import shortId from 'shortid'
 
 const NotificationDB = {
     list: [
-        {
-            id: shortId.generate(),
-            heading: 'Message',
-            icon: {
-                name: 'chat',
-                color: 'primary',
-            },
-            timestamp: 1570702802573,
-            title: 'New message from Devid',
-            subtitle: 'Hello, Any progress...',
-            path: 'chat',
-        },
-        {
-            id: shortId.generate(),
-            heading: 'Alert',
-            icon: {
-                name: 'notifications',
-                color: 'error',
-            },
-            timestamp: 1570702702573,
-            title: 'Server overloaded',
-            subtitle: 'Traffice reached 2M',
-            path: 'page-layouts/user-profile',
-        },
-        {
-            id: shortId.generate(),
-            heading: 'Message',
-            icon: {
-                name: 'chat',
-                color: 'primary',
-            },
-            timestamp: 1570502502573,
-            title: 'New message from Goustove',
-            subtitle: 'Hello, send me details',
-            path: 'chat',
-        },
+        // {
+        //     id: shortId.generate(),
+        //     heading: 'Message',
+        //     icon: {
+        //         name: 'chat',
+        //         color: 'primary',
+        //     },
+        //     timestamp: 1570702802573,
+        //     title: 'New message from Devid',
+        //     subtitle: 'Hello, Any progress...',
+        //     path: 'chat',
+        // },
+        // {
+        //     id: shortId.generate(),
+        //     heading: 'Alert',
+        //     icon: {
+        //         name: 'notifications',
+        //         color: 'error',
+        //     },
+        //     timestamp: 1570702702573,
+        //     title: 'Server overloaded',
+        //     subtitle: 'Traffice reached 2M',
+        //     path: 'page-layouts/user-profile',
+        // },
+        // {
+        //     id: shortId.generate(),
+        //     heading: 'Message',
+        //     icon: {
+        //         name: 'chat',
+        //         color: 'primary',
+        //     },
+        //     timestamp: 1570502502573,
+        //     title: 'New message from Goustove',
+        //     subtitle: 'Hello, send me details',
+        //     path: 'chat',
+        // },
     ],
 }
 
